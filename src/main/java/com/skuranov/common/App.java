b328d2b6d46dd0cbd55c6c70d1e5520c78b01844
SKuranov <skuranov@gmail.com>
2015-09-14 21:27:21 +0300

    version with maven-based jar

--- src/main/java/com/skuranov/common/App.java
+++ src/main/java/com/skuranov/common/App.java
@@ -23,92 +23,6 @@ package com.skuranov.common;
 public class App
 {
     public static void main( String[] args ) throws SOAPException, TransformerException {
-        SOAPMessage soapMessage = messageGen();
-        //Отправка сообщения и получение ответа
-        //Установка адресата
-        String destination = "http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx";
-        //Отправка
-        SOAPConnectionFactory soapConnFactory = SOAPConnectionFactory.newInstance();
-        SOAPConnection connection = soapConnFactory.createConnection();
-        SOAPMessage reply = connection.call(soapMessage, destination);
-
-        //ответ
-        //Проверка полученного ответа
-        System.out.println("\nRESPONSE:\n");
-        //Создание XSLT-процессора
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        //Получение содержимого ответа
-        Source sourceContent = reply.getSOAPPart().getContent();
-        //Задание выходного потока для результата преобразования
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        StreamResult result = new StreamResult(os);
-        transformer.transform(sourceContent, result);
-        byte tempArr[] = os.toByteArray();
-        ByteArrayInputStream is = new ByteArrayInputStream(tempArr);
-
-        try {
-
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser saxParser = factory.newSAXParser();
-
-
-            DefaultHandler handler = new DefaultHandler() {
-
-
-            boolean rateUSDExist = false;
-
-
-            public void startElement(String uri, String localName,String qName,
-                                     Attributes attributes) throws SAXException
-            {
-
-
-                if (qName.equalsIgnoreCase("USD")) {
-                    rateUSDExist = true;
-                    System.out.print("USD Rate: ");
-                }
-
-
-            }
-
-            public void characters(char ch[], int start, int length) throws SAXException {
-
-                if (rateUSDExist) {
-                    System.out.println(new String(ch, start, length) + "    ");
-                    rateUSDExist = false;
-
-                    System.out.println(new java.sql.Timestamp(System.currentTimeMillis()));
-                    Session session = HibernateUtil.getSessionFactory().openSession();
-
-                    session.beginTransaction();
-                    Rate rate = new Rate();
-
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yy:HH:mm:SS");
-                    String curDateTime = simpleDateFormat.format(System.currentTimeMillis());
-
-                    rate.setRateTime(curDateTime);
-                    rate.setUSDRate(new String(ch, start, length) );
-
-                    session.save(rate);
-                    session.getTransaction().commit();
-
-                }
-
-            }
-
-        };
-
-            saxParser.parse(is, handler);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private static SOAPMessage messageGen() throws SOAPException
-    {
-
         SOAPMessage soapMessage = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL).createMessage();
         SOAPPart part = soapMessage.getSOAPPart();
         SOAPEnvelope envelope = part.getEnvelope();
@@ -116,8 +30,56 @@ public class App
         Name bodyName = envelope.createName("AllDataInfoXML", null, "http://web.cbr.ru/");
         body.addBodyElement(bodyName);
         soapMessage.saveChanges();
-        //this.writeXMLtoConsole(soapMessage);
-        return soapMessage;
+        String destination = "http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx";
+        SOAPConnectionFactory soapConnFactory = SOAPConnectionFactory.newInstance();
+        SOAPConnection connection = soapConnFactory.createConnection();
+        SOAPMessage reply = connection.call(soapMessage, destination);
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        Source sourceContent = reply.getSOAPPart().getContent();
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        StreamResult result = new StreamResult(os);
+        transformer.transform(sourceContent, result);
+        byte tempArr[] = os.toByteArray();
+        ByteArrayInputStream is = new ByteArrayInputStream(tempArr);
+
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            DefaultHandler handler = new DefaultHandler() {
+                boolean rateUSDExist = false;
+                public void startElement(String uri, String localName,String qName,Attributes attributes)
+                        throws SAXException{
+                    if (qName.equalsIgnoreCase("USD")) {
+                        rateUSDExist = true;
+                        System.out.print("USD Rate: ");
+                    }
+                }
+
+                public void characters(char ch[], int start, int length) throws SAXException {
+                    if (rateUSDExist) {
+                        System.out.println(new String(ch, start, length) + "    ");
+                        rateUSDExist = false;
+                        System.out.println(new java.sql.Timestamp(System.currentTimeMillis()));
+                        Session session = HibernateUtil.getSessionFactory().openSession();
+                        session.beginTransaction();
+                        Rate rate = new Rate();
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yy:HH:mm:SS");
+                        String curDateTime = simpleDateFormat.format(System.currentTimeMillis());
+                        rate.setRateTime(curDateTime);
+                        rate.setUSDRate(new String(ch, start, length) );
+                        session.save(rate);
+                        session.getTransaction().commit();
+                    }
+                }
+            };
+
+        saxParser.parse(is, handler);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
